7d19fa261204bf0623d6520148eb1a26ff0a65ed
iaulitin <iaulitin@yandex.ru>
2024-11-16 14:56:44 +0100

    Netty client and server

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.0")
+    implementation("io.netty:netty-all:4.1.108.Final")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ src/main/java/org/iaulitin/_06_netty_client_and_server/NettyServer.java
@@ -0,0 +1,57 @@
+package org.iaulitin._06_netty_client_and_server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.iaulitin.Constants;
+
+import java.nio.charset.StandardCharsets;
+
+public class NettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(Constants.SERVER_PORT).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    static class EchoServerHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+            System.out.println("Server received: " + ((ByteBuf) msg).toString(StandardCharsets.UTF_8));
+            ctx.write(msg);
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) {
+            ctx.flush();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+}
